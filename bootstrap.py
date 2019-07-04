922496e3347d8f48ec6f9f7f99c8b83441680f55
Rudolf <52538271+RudSmo@users.noreply.github.com>
2019-07-04 16:13:02 +0200

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,28 +2,17 @@ import numpy as np
 import pandas as pd
 
 
-def bootstrap_resample(X, n=None):
-    """ Bootstrap resample an array_like
-    Parameters
-    ----------
-    X : array_like
-      data to resample
-    n : int, optional
-      length of resampled array, equal to len(X) if n==None
-    Results
-    -------
-    returns X_resamples
-    """
+def resample(X,n):
     if n == None:
         n = len(X)
-        
-    resample_i = np.floor(np.random.rand(n)*len(X)).astype(int)
-    X_resample = X[resample_i]
-    return X_resample
+    res = np.floor(np.random.rand(n)*len(X)).astype(int)
+    X_r = X[res]
+    return X_r
 
 size=50000
+
 X = np.genfromtxt("test_data.txt",delimiter=",")[:,1]
-X_re = bootstrap_resample(X,size)
+X_r = resample(X,size)
 f = open("test_data_re.txt","w")
 for i in range(size):
  f.write(",%s\n"%(X_re[i]))
