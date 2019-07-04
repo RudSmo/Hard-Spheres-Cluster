4e67d1d9e1e3275508869818443e2572521837a8
Rudolf <52538271+RudSmo@users.noreply.github.com>
2019-07-04 16:55:35 +0200

    Add files via upload

---
+++ statistics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+X = np.genfromtxt("test_data_re.txt",delimiter=",")[:,1]
+
+NPar=10
+S = np.std(X)
+M = np.average(X)
+
+f = open("avg.txt","w")
+f.write("%s,%s,%s"%(NPar,M,S))
