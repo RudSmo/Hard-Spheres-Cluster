698a3b88a601912b69b3c17b4e45eba8c59c918c
Rudolf <52538271+RudSmo@users.noreply.github.com>
2019-07-06 03:04:12 +0200

    Add files via upload

--- scrpt.sh
+++ scrpt.sh
@@ -1,19 +1,20 @@
 #/bin/bash
 
-for k in {10..60..50}; do
-  mkdir N${k};
-  cp src.py N${k}/;
-  cp statistics.py N${k}/;
-  cp bootstrap.py N${k}/;
-  cd N${k}/;
+for k in {1..300..5}; do
+  mkdir data/v3/N${k};
+  cp source/src.py data/v3/N${k}/;
+  cp source/statistics.py data/v3/N${k}/;
+  cp source/bootstrap.py data/v3/N${k}/;
+  cd data/v3/N${k}/;
    sed "s/^NPar=.*$/NPar=${k}/g" src.py > src${k}.py;
    rm src.py;
    for j in {1..100}; do
-    mkdir $j/;
-    cp src${k}.py $j/;
+    mkdir v$j/;
+    cp src${k}.py v$j/;
     python3 src${k}.py;
     wait;
    done;
+   rm -rf v* ; 
    python3 bootstrap.py;
    wait;
    sed "s/^NPar=.*$/NPar=${k}/g" statistics.py > statistics${k}.py
@@ -21,6 +22,8 @@ for k in {10..60..50}; do
    python3 statistics${k}.py
   wait;
   paste avg.txt >> ../outfile.txt
+ cd ../;
+ cd ../;
  cd ../;
  wait;
 done
